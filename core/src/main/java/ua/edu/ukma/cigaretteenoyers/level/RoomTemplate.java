0d08ea4733beaebcabe300516075da5bc01a8fd1
Demkiv Maxym <demkivmaxym@gmail.com>
2026-06-04 15:54:42 +0300

    feat: init room templates and factory for slum level

---
+++ core/src/main/java/ua/edu/ukma/cigaretteenoyers/level/RoomTemplate.java
@@ -0,0 +1,58 @@
+package ua.edu.ukma.cigaretteenoyers.level;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class RoomTemplate {
+    public static final int WIDTH = 16;
+    public static final int HEIGHT = 11;
+    public static final float TILE_SIZE = 32f; // Розмір одного тайла в пікселях
+
+    private final int[][] tileMap;
+    private final Array<Vector2> enemySpawnPoints;
+    private final Vector2 slotMachineSpawnPoint;
+
+    public RoomTemplate(int[][] matrix) {
+        this.tileMap = matrix;
+        this.enemySpawnPoints = new Array<>();
+        this.slotMachineSpawnPoint = new Vector2(WIDTH * TILE_SIZE / 2, HEIGHT * TILE_SIZE / 2);
+        parseMatrix();
+    }
+
+    private void parseMatrix() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                int tileType = tileMap[y][x];
+
+                float worldX = x * TILE_SIZE;
+                float worldY = (HEIGHT - 1 - y) * TILE_SIZE;
+
+                if (tileType == 3) {
+                    enemySpawnPoints.add(new Vector2(worldX, worldY));
+                } else if (tileType == 4) {
+                    slotMachineSpawnPoint.set(worldX, worldY);
+                }
+            }
+        }
+    }
+
+    public int[][] getTileMap() { return tileMap; }
+    public Array<Vector2> getEnemySpawnPoints() { return enemySpawnPoints; }
+    public Vector2 getSlotMachineSpawnPoint() { return slotMachineSpawnPoint; }
+
+    public Array<Rectangle> getWallColliders() {
+        Array<Rectangle> colliders = new Array<>();
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+
+                if (tileMap[y][x] == 1 || tileMap[y][x] == 2) {
+                    float worldX = x * TILE_SIZE;
+                    float worldY = (HEIGHT - 1 - y) * TILE_SIZE;
+                    colliders.add(new Rectangle(worldX, worldY, TILE_SIZE, TILE_SIZE));
+                }
+            }
+        }
+        return colliders;
+    }
+}
