a846656aab6fde6e9810648a324d870ecbef57ae
Demkiv Maxym <demkivmaxym@gmail.com>
2026-06-04 16:28:07 +0300

    feat: add ShapeRenderer room visualization and dynamic door collision system

--- core/src/main/java/ua/edu/ukma/cigaretteenoyers/level/RoomInstance.java
+++ core/src/main/java/ua/edu/ukma/cigaretteenoyers/level/RoomInstance.java
@@ -1,5 +1,7 @@
 package ua.edu.ukma.cigaretteenoyers.level;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
@@ -17,18 +19,25 @@ public class RoomInstance {
     private int remainingEnemies;
     private boolean slotMachineSpawned;
 
+    private final Rectangle leftDoorCollider;
+    private final Rectangle rightDoorCollider;
+
     public RoomInstance(RoomTemplate template) {
         this.template = template;
         this.currentState = RoomState.NOT_VISITED;
         this.remainingEnemies = template.getEnemySpawnPoints().size;
         this.slotMachineSpawned = false;
+
+        float midY = (RoomTemplate.HEIGHT / 2) * RoomTemplate.TILE_SIZE;
+
+        this.leftDoorCollider = new Rectangle(0, midY, RoomTemplate.TILE_SIZE, RoomTemplate.TILE_SIZE);
+        this.rightDoorCollider = new Rectangle((RoomTemplate.WIDTH - 1) * RoomTemplate.TILE_SIZE, midY, RoomTemplate.TILE_SIZE, RoomTemplate.TILE_SIZE);
     }
 
     public void enterRoom() {
         if (currentState == RoomState.NOT_VISITED) {
             currentState = RoomState.ACTIVE;
             System.out.println("Gamer enter into room, enemy count: " + remainingEnemies);
-
             if (remainingEnemies == 0) {
                 clearRoom();
             }
@@ -38,7 +47,6 @@ public class RoomInstance {
     public void update(int currentAliveEnemiesCount) {
         if (currentState == RoomState.ACTIVE) {
             this.remainingEnemies = currentAliveEnemiesCount;
-
             if (remainingEnemies <= 0) {
                 clearRoom();
             }
@@ -48,7 +56,6 @@ public class RoomInstance {
     private void clearRoom() {
         currentState = RoomState.CLEARED;
         System.out.println("Room is clear. the doors are open");
-
         if (!slotMachineSpawned) {
             triggerSlotMachineSpawn();
         }
@@ -58,16 +65,52 @@ public class RoomInstance {
         slotMachineSpawned = true;
         Vector2 spawnPos = template.getSlotMachineSpawnPoint();
         System.out.println("Slot machine xy: " + spawnPos);
-
     }
 
+    public void render(ShapeRenderer shapeRenderer) {
+        int[][] tileMap = template.getTileMap();
 
-    public RoomState getCurrentState() { return currentState; }
-    public RoomTemplate getTemplate() { return template; }
-    public int getRemainingEnemies() { return remainingEnemies; }
-    public boolean isSlotMachineSpawned() { return slotMachineSpawned; }
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        for (int y = 0; y < RoomTemplate.HEIGHT; y++) {
+            for (int x = 0; x < RoomTemplate.WIDTH; x++) {
+                int tileType = tileMap[y][x];
+                float worldX = x * RoomTemplate.TILE_SIZE;
+                float worldY = (RoomTemplate.HEIGHT - 1 - y) * RoomTemplate.TILE_SIZE;
+
+                if (tileType == 1) {
+                    shapeRenderer.setColor(Color.GRAY); // wall
+                } else if (tileType == 2) {
+                    shapeRenderer.setColor(Color.BROWN); // obstacle
+                } else {
+                    shapeRenderer.setColor(Color.DARK_GRAY); // floor
+                }
+                shapeRenderer.rect(worldX, worldY, RoomTemplate.TILE_SIZE, RoomTemplate.TILE_SIZE);
+            }
+        }
+
+        if (currentState == RoomState.ACTIVE) {
+            shapeRenderer.setColor(Color.RED); // door closed
+        } else {
+            shapeRenderer.setColor(Color.GREEN); // door open
+        }
+
+        shapeRenderer.rect(leftDoorCollider.x, leftDoorCollider.y, leftDoorCollider.width, leftDoorCollider.height);
+        shapeRenderer.rect(rightDoorCollider.x, rightDoorCollider.y, rightDoorCollider.width, rightDoorCollider.height);
+
+        shapeRenderer.end();
+    }
 
     public Array<Rectangle> getWallColliders() {
-        return template.getWallColliders();
+        Array<Rectangle> colliders = template.getWallColliders();
+        if (currentState == RoomState.ACTIVE) {
+            colliders.add(leftDoorCollider);
+            colliders.add(rightDoorCollider);
+        }
+        return colliders;
     }
+
+    public Rectangle getLeftDoorCollider() { return leftDoorCollider; }
+    public Rectangle getRightDoorCollider() { return rightDoorCollider; }
+    public RoomState getCurrentState() { return currentState; }
+    public RoomTemplate getTemplate() { return template; }
 }
