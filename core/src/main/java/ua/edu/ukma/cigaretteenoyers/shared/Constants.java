ebd240f83f0c998e296e6f9908f95da4359978ab
Mykola Vashchuk <mmykolavashchuk@gmail.com>
2026-06-07 18:30:42 +0300

    feat: player model, movement and project structure

---
+++ core/src/main/java/ua/edu/ukma/cigaretteenoyers/shared/Constants.java
@@ -0,0 +1,21 @@
+package ua.edu.ukma.cigaretteenoyers.shared;
+
+public class Constants {
+    public static final float WORLD_WIDTH   = 20f;
+    public static final float WORLD_HEIGHT  = 11.25f;
+
+    public static final float PLAYER_SPEED  = 6f;
+    public static final float PLAYER_RADIUS = 0.4f;
+    public static final float IFRAMES_TIME  = 1.0f;
+    public static final int   START_MONEY   = 150;
+
+    public static final float BULLET_SPEED  = 14f;
+    public static final float PISTOL_RATE   = 0.35f;
+    public static final int   PISTOL_COST   = 1;
+    public static final int   PISTOL_DAMAGE = 25;
+    public static final int   SHOTGUN_COST  = 8;
+    public static final int   SHOTGUN_DAMAGE = 15;
+    public static final int   SHOTGUN_PELLETS = 5;
+    public static final float SHOTGUN_SPREAD  = 20f;
+    public static final float SHOTGUN_RATE    = 0.8f;
+}
