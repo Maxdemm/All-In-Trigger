53f80f0bcbca9fe63fbfc6955f9180c3686f667e
Dmytro <poplavskijdmitro584@gmail.com>
2026-06-08 15:44:48 +0300

    Save cnahges

--- core/src/main/java/ua/edu/ukma/cigaretteenoyers/shared/IEconomyService.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.ukma.cigaretteenoyers.shared;
-
-public interface IEconomyService {
-    boolean trySpend(int amount);
-    void    addMoney(int amount);
-    int     getBalance();
-    boolean isBankrupt();
-}
